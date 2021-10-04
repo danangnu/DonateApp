e078661682fc00604a23c97ba98915129e50e0b0
Danang Nurdiansyah <nattesc82@gmail.com>
2021-10-04 22:20:22 +0800

    master

--- client/src/app/missing/missing-detail/missing-detail.component.ts
+++ client/src/app/missing/missing-detail/missing-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
 import { Missing } from 'src/app/_models/missing';
 import { MissingsService } from 'src/app/_services/missings.service';
 
@@ -10,16 +11,41 @@ import { MissingsService } from 'src/app/_services/missings.service';
 })
 export class MissingDetailComponent implements OnInit {
   missing: Missing;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
 
   constructor(private missingService: MissingsService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.loadMissing();
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ]
+  }
+
+  getImages(): NgxGalleryImage[] {
+    const imageUrls = [];
+    for (const photo of this.missing.photos) {
+      imageUrls.push({
+        small: photo?.url,
+        medium: photo?.url,
+        big: photo?.url
+      })
+    }
+    return imageUrls;
   }
 
   loadMissing() {
     this.missingService.getMissing(this.route.snapshot.paramMap.get('id')).subscribe(missing => {
       this.missing = missing;
+      this.galleryImages = this.getImages();
     });
   }
 }
